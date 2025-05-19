f312f7cf0f1afeee58f0149322d8282821cdbd42
omsingh10 <os0106897@gmail.com>
2025-05-19 10:58:40 +0530

    ifelse1

---
+++ ifelse1.py
@@ -0,0 +1,14 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = int(input("Enter a number: "))
+
+if a==7:
+    n = b*c
+    print(n)
+elif b==7:
+    print(c)
+elif c==7:
+    print(-1)
+else:
+    k=a*b*c
+    print(k)
---
+++ ifelse2.py
@@ -0,0 +1,11 @@
+#check if buzzz and fuzz
+a = int(input("Enter a number: "))
+
+if a % 3 == 0 and a % 5 == 0:
+    print("fizzbuzz")
+elif a % 3 == 0:
+    print("fizz")
+elif a % 5 == 0:
+    print("buzz")
+else:
+    print(a)
