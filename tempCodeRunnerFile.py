436d3d4152f60c46c26da3e3ec29a5fd0b3d00ba
omsingh10 <os0106897@gmail.com>
2025-05-21 11:11:26 +0530

    human-3

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,8 @@
-a=[1, 3, 3, 4, 5]
-# b = []
-# for i in a:
-#     b.append(i**2)
-# print(b)
+lst=[ 0 ,1,1,2,3,5,8,13,21,34]
+# lst[-8] = 10
+# print(lst)
+
+# num = [ 1 ,2,3,4,5,6,7,8]
+# num[1:]
+# print(num[:3])
+# pri
